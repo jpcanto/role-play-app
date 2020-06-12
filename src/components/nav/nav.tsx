7894c06fddb6f03eefb9a2aef678b87eaf9f30c4
jpcanto <jplopessk8@gmail.com>
2020-06-12 00:57:32 -0300

    [ UPDATE ] - navigation / user info field

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import userImage from '../../img/user.png';
 import Messages from '../../data/messages.json';
-import { Navigation } from './styled';
+import { Navigation, User } from './styled';
 
 const Nav: React.FC = () => {
 
@@ -10,6 +11,9 @@ const Nav: React.FC = () => {
     return (
         <Navigation>
             <Link to="/main">Home</Link>
+            <User image={userImage}>
+                <div></div>
+            </User>
         </Navigation>
     );
 };
